b5bcf760b920385a8c728b71737c543b1bd629a6
mute1997 <mute1008@gmail.com>
2017-11-30 02:17:00 +0900

    add preprocessing.py

--- main.py
+++ main.py
@@ -2,32 +2,47 @@ import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.externals import joblib
 
-filename = './data/coincheckJPY_1-min_data_2014-10-31_to_2017-10-20.csv'
+filename = './data/preprocessed_data_2014-10-31_to_2017-10-20.csv'
 ratio = 0.7 # 学習用データの割合
 
 def get_lines():
     with open(filename, 'r') as f:
         return len(f.readlines())
 
+def get_data(ary):
+    try:
+        open_ = ary[0]
+        high = ary[1]
+        low = ary[2]
+        close = ary[3]
+        return open_, high, low, close, 0
+    except:
+        return -1, -1, -1, -1, -1
+
 def train_data():
     train_X = []
     train_y = []
     with open(filename, 'r') as f:
         f.readline()
-        last_time_price = list(map(float, f.readline().replace('\n','').split(',')))[4]
-        ary = list(map(float, f.readline().replace('\n','').split(',')))
+        _, _, _, close, _ = get_data(list(map(float, f.readline().replace('\n','').split(','))))
+        last_time_price = close
 
         # 上がってたら1, 下がってたら0, 変わらないなら2
+        open_, high, low, close, _ = get_data(list(map(int, f.readline().replace('\n','').split(','))))
         for i in range(0, int(get_lines() * ratio)):
-            train_X.append(ary)
-            if last_time_price < ary[4]:
+            if _ == -1:
+                return np.array(train_X), np.array(train_y)
+
+            train_X.append([open_, high, low, close])
+            if last_time_price < close:
                 train_y.append(1)
-            elif last_time_price == ary[4]:
+            elif last_time_price == close:
                 train_y.append(2)
             else:
                 train_y.append(0)
 
-            last_time_price = ary[4]
+            last_time_price = close
+            open_, high, low, close, _ = get_data(list(map(int, f.readline().replace('\n','').split(','))))
     return np.array(train_X), np.array(train_y)
 
 def save_model(clf, filename):
@@ -44,6 +59,12 @@ if __name__ == '__main__':
     clf.fit(train_X, train_y)
     save_model(clf, 'clf.pkl')
 
-    # data = np.array([[1508457540,637559,637997,636547,637169,27.9909433,17844185.365,637498.53565]])
-    # clf = resume_model(model)
-    # print(clf.predict(np.array(data)))
+    data = np.array([[626668,634030,570000,585681]])
+    clf = resume_model(model)
+
+    # TODO: modelの評価をする
+    print(data)
+    print(clf.predict(data))
+
+    print([[84634, 85389, 84542, 85075]])
+    print(clf.predict(np.array([[84634, 85389, 84542, 85075]])))
--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,50 @@
 # Timestamp,Open,High,Low,Close,Volume_(BTC),Volume_(Currency),Weighted_Price
-# TODO: 1日ごとに丸める
-# TODO: Timestampを削除する
+# TODO: 移動平均線など他の数値もデータに含める
+# 1日ごとに丸める -> テクニカル分析の数値を計算 -> csv吐き出し
 
-with open('./data/coincheckJPY_1-min_data_2014-10-31_to_2017-10-20.csv', 'r') as f:
-    pass
+import datetime
 
-with open('./data/preprocessed_data_2014-10-31_to_2017-10-20.csv', 'w') as f:
-    pass
+def get_prices(ary):
+    try:
+        open_ = int(ary[1])
+        close = int(ary[4])
+        high = int(ary[2])
+        low = int(ary[3])
+        timestamp = datetime.datetime.fromtimestamp(int(ary[0])).strftime('%Y%m%d')
+        return open_, close, high, low, timestamp, 0
+    except:
+        return -1, -1, -1, -1, -1, -1
+
+def round_data():
+    data = []
+    data.append(['open', 'high', 'low', 'close'])
+    with open('./data/coincheckJPY_1-min_data_2014-10-31_to_2017-10-20.csv', 'r') as f:
+        f.readline()
+        open_, close, high, low, timestamp, _ = get_prices(f.readline().split(','))
+        while _ != -1:
+            date = timestamp
+            high_ = -9999999
+            low_ = 99999999
+            open_, _, high, low, timestamp, _ = get_prices(f.readline().split(','))
+            while date == timestamp:
+                if high_ < high:
+                    high_ = high
+                if low_ > low:
+                    low_ = low
+                _, _, high, low, timestamp, _ = get_prices(f.readline().split(','))
+                if _ == -1:
+                    break
+
+            if _ != -1:
+                _, close, _, _, _, _ = get_prices(f.readline().split(','))
+                data.append([open_, high_, low_, close])
+        return data
+
+def write_data(data):
+    with open('./data/preprocessed_data_2014-10-31_to_2017-10-20.csv', 'w') as f:
+        for i in data:
+            f.write(','.join(list(map(str, i)))+'\n')
+
+if __name__ == '__main__':
+    data = round_data()
+    write_data(data)
