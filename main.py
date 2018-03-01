50951e845984e9f6ba09b2ebcf49a93c52777fd5
mute1997 <mute1008@gmail.com>
2018-03-02 01:57:46 +0900

    add scaler

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from sklearn import preprocessing
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 from model import save_model, resume_model
 
 filename = 'bitflyer.json'
 model_name = 'clf.pkl'
 
-def get_return_index(prices):
-    return_index = []
-    return_index = (1 + pandas.Series(prices).pct_change()).cumprod()
-    return_index[0] = 1
-    return return_index
+def scale_standard(ary):
+    sc = StandardScaler()
+    sc.fit(ary)
+    return sc.transform(ary)
 
 # 取得したデータのAPIドキュメント
 # https://cryptowatch.jp/docs/api#ohlc
@@ -31,17 +31,14 @@ def get_train_data():
 
     # 終値のみを取り出す
     prices = []
-    for value in order_book_data['result']['900']:
+    for value in order_book_data['result']['60']:
         prices.append(value[1])
 
-    # pricesをreturn indexにする
-    return_index = get_return_index(prices)
-
     # データの生成
-    input_amount = 10
-    for index in range(0, len(return_index) - input_amount, input_amount):
-        x = return_index[index:index+input_amount].tolist()
-        y = 1 if x[-1] < return_index[index+input_amount+1] else 0 # 上がってたら1, 下がってる or 同じだったら0
+    input_amount = 5
+    for index in range(0, len(prices) - input_amount, input_amount):
+        x = prices[index:index+input_amount]
+        y = 1 if x[-1] < prices[index+input_amount+1] else 0 # 上がってたら1, 下がってる or 同じだったら0
         train_X.append(x)
         train_y.append(y)
 
@@ -54,7 +51,13 @@ if __name__ == '__main__':
     # これを参考にスコア算出 -> (https://qiita.com/Lewuathe/items/09d07d3ff366e0dd6b24)
     # ----------------------------------------------------
     train_features, train_labels = get_train_data()
-    train_X, test_X, train_y, test_y = train_test_split(train_features, train_labels, test_size=0.2, random_state=0)
+    train_X, test_X, train_y, test_y = train_test_split(train_features,
+                                                        train_labels,
+                                                        test_size=0.2,
+                                                        random_state=0)
+    # train_Xとtest_Xの標準化
+    train_X = scale_standard(train_X)
+    test_X = scale_standard(test_X)
 
     clf = MLPClassifier()
     clf.fit(train_X, train_y)
